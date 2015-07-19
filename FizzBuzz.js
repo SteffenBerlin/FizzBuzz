6d2033943313516fd98a9730d410030816679b77
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2015-07-19 14:12:56 +0200

    Add output

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,16 @@
-var text;
-for (var i = start; i <= end; i++) {
-	if (i % 3 == 0 && i % 5 == 0)
-		text += "FizzBuzz\n";
-	else if (i % 3 == 0)
-		text += "Fizz\n";
-	else if (i % 5 == 0)
-		text += "Buzz\n";
-	else
-		text = text + i + "\n";
-}
-console.log(text);
+var printFizzBuzz = function(start, end) {
+	var text = "";
+	for (var i = start; i <= end; i++) {
+		if (i % 3 == 0 && i % 5 == 0)
+			text += "FizzBuzz<br>";
+		else if (i % 3 == 0)
+			text += "Fizz<br>";
+		else if (i % 5 == 0)
+			text += "Buzz<br>";
+		else
+			text = text + i + "<br>";
+	}
+document.getElementById("output").innerHTML = text;
+
+};
+
